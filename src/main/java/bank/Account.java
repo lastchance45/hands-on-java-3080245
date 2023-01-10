5378a76b30a6821de108f9417dc7d68b6630b4c1
lastchance45 <122393769+lastchance45@users.noreply.github.com>
2023-01-10 18:51:07 +0000

    account class built

---
+++ src/main/java/bank/Account.java
@@ -0,0 +1,37 @@
+package bank;
+
+public class Account {
+  private int id;
+  private String accountType;
+  private double balance;
+
+  public Account(int id, String accountType, double balance){
+  setId(id);
+  setAccountType(accountType);
+  setBalance(balance);
+}
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getAccountType() {
+    return this.accountType;
+  }
+
+  public void setAccountType(String accountType) {
+    this.accountType = accountType;
+  }
+
+  public double getBalance() {
+    return this.balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+
+}
--- src/main/java/bank/Customer.java
+++ src/main/java/bank/Customer.java
@@ -1,7 +1,5 @@
 package bank;
 
-import java.lang.reflect.Constructor;
-
 public class Customer {
   private int id;
   private String name;
