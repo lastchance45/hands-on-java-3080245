b7177571b89813d1deafec66cd00a78018dcda14
lastchance45 <122393769+lastchance45@users.noreply.github.com>
2023-01-10 19:12:54 +0000

    pulled data from database

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,7 +2,9 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 public class DataSource {
 
@@ -17,7 +19,27 @@ public class DataSource {
   }
   return connection;
 }
+  public static Customer getCustomer(String username){
+    String sql = "select * from customers where username = ?";
+    Customer customer = null;
+    try(Connection connection = connect();
+    PreparedStatement statement = connection.prepareStatement(sql)){
+      statement.setString(1, username);
+      try(ResultSet resultSet = statement.executeQuery()){
+        customer = new Customer(
+          resultSet.getInt("id"),
+          resultSet.getString("name"),
+          resultSet.getString("username"),
+          resultSet.getString("password"),
+          resultSet.getInt("account_id"));
+      }
+    } catch(SQLException e){
+      e.printStackTrace();
+    }
+    return customer;
+  }
   public static void main(String[] args){
-    connect();
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    System.out.println(customer.getName());
   }
 }
